dd77101a43436faccb87ce4c1247ea6e08e6bbda
Kyle Van Bergen <kyleavb@gmail.com>
2018-06-12 13:38:24 -0700

    intigrated uploading new files to cloudinary and pushing current uploaded files to display on front page

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+require("dotenv").config();
 var ejsLayouts = require('express-ejs-layouts');
 var path = require('path');
+var multer = require('multer');
+var upload = multer({dest: './uploads/'});
+var cloudinary = require('cloudinary');
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -12,13 +16,22 @@ var images = [];
 app.get('/', function(req, res){
     console.log('root get request hit')
     // res.send({thing:'Just made a simple change'})
-    res.render('index', {images: images});
+    res.render('index', {cloudinary, images: images});
 })
 
 app.get('/upload', function(req,res){
     console.log('upload get route hit!');
     res.render('upload');
 });
+
+app.post('/upload',upload.single('myFile'), function(req, res){
+    console.log(req.file.path)
+    cloudinary.uploader.upload(req.file.path, function(result){
+        console.log(result);
+        images.push(result.public_id);
+        res.redirect('/');
+    })
+})
 
 app.get('*', function(req, res){
     res.send("Well this is awkward, you went to a place without a 404...You're doing so good!")
